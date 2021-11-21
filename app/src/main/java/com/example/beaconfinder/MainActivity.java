0dbb2571796383b9c76691f92d840dd0c15f0d2a
Alex G <giasson.alexj@gmail.com>
2021-11-21 10:31:28 -0800

    send request

--- app/src/main/java/com/example/beaconfinder/MainActivity.java
+++ app/src/main/java/com/example/beaconfinder/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.beaconfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    Button putBtn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        putBtn = findViewById(R.id.putBtn);
+
+        putBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // make http PUT request to server
+                RequestQueue queue;
+                String url;
+                StringRequest stringRequest;
+
+                queue = Volley.newRequestQueue(MainActivity.this);
+                url = "http://localhost:7123";
+                stringRequest = new StringRequest(Request.Method.PUT, url,
+                    (response) -> {
+                        System.out.println(response);
+                        tv.setText(response.toString());
+                    },
+                    (error) -> {
+                        System.out.println(error);
+                        tv.setText(error.toString());
+                    }
+                );
+                queue.add(stringRequest);
+
+
+                //display toast
+                Toast.makeText(MainActivity.this, "Request sent", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
